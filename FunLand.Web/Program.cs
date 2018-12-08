8afad072a2cfa5467494b83f5feb193bfa7f1f81
silvercrown07 <44765224+silvercrown07@users.noreply.github.com>
2018-12-08 19:15:05 +0800

    exclude certain files and folder from publish use ASPNETCORE_URLS instead of hard code

--- FunLand.Web/Program.cs
+++ FunLand.Web/Program.cs
@@ -20,7 +20,6 @@ namespace FunLand.Web
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-                .UseKestrel(options => options.Listen(IPAddress.Loopback, 5000));
+                .UseStartup<Startup>();
     }
 }
